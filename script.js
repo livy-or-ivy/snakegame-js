345f795aab7ac9c2201eb744fb8eb7f1137ba018
rosiee <97999463+HeyRosiee@users.noreply.github.com>
2022-08-07 14:28:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 var getCanvas = document.getElementById("game");
 var canvas = getCanvas.getContext("2d");
 
+let gameValue = 0
 const settings = {
     fruits: {
-        total: 15
+        total: 25
     },
     map: {
         width: getCanvas.width,
         height: getCanvas.height
     },
     player: {
+        velocity: 170,
         points: 0,
+        snake: 0,
         x: 0,
         y: 0
     },
     color: {
         map: "white",
-        player: "black"
+        player: "#75ff73"
     },
     keyboard: {
         lastKey: '0'
@@ -28,109 +31,183 @@ const fruitsDB = {
     count: 0,
     ready: false
 };
+
 const fruits = fruitsDB || {};
+const snakeDB = {
+    total: 0
+}
+const snake = snakeDB || {};
 
-//system
-function game(type, x, y, color) {
-    if (type == "clearPixel") {
-        canvas.fillStyle = settings.color.map;
-        canvas.fillRect(x, y, 50, 50);
-    } else if (type == "setPixel") {
-        canvas.fillStyle = color;
-        const multiplier = 50 * Number(settings.player.points)
-        canvas.fillRect(x, y, 50, 50);
-    } else if (type == "fruitDetector") {
-        if (fruitsDB.count == 0) {
-            fruitsDB.count = fruitsDB.max
-        } else {
-            const requestFruit = fruits[`fruit${fruitsDB.count}`]
-            const saveFruitID = fruitsDB.count
-            fruitsDB.count = fruitsDB.count - 1
+function play() {
+    function endGame(type) {
+        if (gameValue == 1) {
+            if (type == "player_win") {
+                settings.keyboard.lastKey = undefined;
+                document.getElementById('pontos').innerHTML = `Voce venceu!<br>(!) Pressione F5 para jogar novamente.`
+            } else if (type == "player_lost") {
+                settings.keyboard.lastKey = undefined;
+                document.getElementById('pontos').innerHTML = `Voce perdeu.<br>(!) Pressione F5 para jogar novamente.`
+            }
 
-            if (requestFruit.x == settings.player.x && requestFruit.y == settings.player.y) {
-                settings.player.points = Number(settings.player.points) + 1
-                document.getElementById('pontos').innerHTML = `Pontos: ${Number(settings.player.points)}`
-                fruits[`fruit${saveFruitID}`].x = undefined
+            //canvas.fillStyle = "red"//settings.color.map;
+            canvas.clearRect(0, 0, settings.map.height, settings.map.width);
+        }
+    }
+
+    //edit game status
+    function game(type, x, y, color) {
+        if (settings.player.points == fruitsDB.max) gameValue = gameValue + 1, endGame("player_win");
+
+        if (type == "clearPixel") {
+            canvas.fillStyle = settings.color.map;
+            canvas.fillRect(x, y, 50, 50);
+        } else if (type == "setPixel") {
+            canvas.fillStyle = color;
+            canvas.fillRect(x, y, 50, 50);
+        } else if (type == "fruitDetector") {
+            if (fruitsDB.count == 0) {
+                fruitsDB.count = fruitsDB.max
             } else {
-                game("fruitDetector");
+                const requestFruit = fruits[`fruit${fruitsDB.count}`]
+                const saveFruitID = fruitsDB.count
+                fruitsDB.count = fruitsDB.count - 1
+                if (requestFruit.x == settings.player.x && requestFruit.y == settings.player.y) {
+                    settings.player.points = Number(settings.player.points) + 1
+                    if (settings.player.velocity !== 40) settings.player.velocity = settings.player.velocity - 10
+                    document.getElementById('pontos').innerHTML = `Pontos: ${Number(settings.player.points)}`
+                    fruits[`fruit${saveFruitID}`].x = undefined
+                } else {
+                    game("fruitDetector");
+                }
             }
         }
     }
-}
 
-function keyboardListener(key) {
-    if (key == "s" && settings.player.y !== settings.map.width - 50) {
-        game("clearPixel", settings.player.x, settings.player.y)
-        game("setPixel", settings.player.x, settings.player.y + 50, settings.color.player)
-        settings.player.y = settings.player.y + 50
-        game("fruitDetector");
-    }
+    //wasd system
+    function keyboardListener(key) {
+        if (key == "s") {
+            if (settings.player.y == settings.map.width - 50) return gameValue = gameValue + 1, endGame("player_lost");
 
-    if (key == "w" && settings.player.y !== 0) {
-        game("clearPixel", settings.player.x, settings.player.y)
-        game("setPixel", settings.player.x, settings.player.y - 50, settings.color.player)
-        settings.player.y = settings.player.y - 50
-        game("fruitDetector");
-    }
+            snakeSystem();
+            game("setPixel", settings.player.x, settings.player.y + 50, settings.color.player)
 
-    if (key == "d" && settings.player.x !== settings.map.width - 50) {
-        game("clearPixel", settings.player.x, settings.player.y)
-        game("setPixel", settings.player.x + 50, settings.player.y, settings.color.player)
-        settings.player.x = settings.player.x + 50
-        game("fruitDetector");
-    }
-
-    if (key == "a" && settings.player.x !== 0) {
-        game("clearPixel", settings.player.x, settings.player.y)
-        game("setPixel", settings.player.x - 50, settings.player.y, settings.color.player)
-        settings.player.x = settings.player.x - 50
-        game("fruitDetector");
-    }
-}
-
-function autoWalk() {
-    setTimeout(() => {
-        keyboardListener(settings.keyboard.lastKey)
-        autoWalk();
-    }, 50)
-}
-autoWalk();
-
-//spawn fruits
-function fruitSystem() {
-    if (fruitsDB.ready == true) {
-        if (fruitsDB.count !== 0) {
-            const getFruitID = fruitsDB.count
-            const requestFruit = fruits[`fruit${getFruitID}`]
-
-            game("setPixel", requestFruit.x, requestFruit.y, 'red')
-
-            fruitsDB.count = fruitsDB.count - 1
-            fruitSystem();
-        }
-    } else {
-        if (settings.fruits.total !== 0) {
-            settings.fruits.total = settings.fruits.total - 1
-            fruitsDB.count = fruitsDB.count + 1
-
-            fruits[`fruit${fruitsDB.count}`] = {
-                x: Math.floor(Math.random() * settings.map.height / 50) * 50,
-                y: Math.floor(Math.random() * settings.map.height / 50) * 50
+            settings.player.snake = settings.player.snake + 1
+            snake[`position${settings.player.snake + 1}`] = {
+                x: settings.player.x,
+                y: settings.player.y + 50
             }
-            fruitSystem()
+            snakeDB.total = snakeDB.total + 1
+
+            settings.player.y = settings.player.y + 50
+            game("fruitDetector");
+        }
+        if (key == "w") {
+            if (settings.player.y == 0) return gameValue = gameValue + 1, endGame("player_lost");
+
+            snakeSystem();
+            game("setPixel", settings.player.x, settings.player.y - 50, settings.color.player)
+
+            settings.player.snake = settings.player.snake + 1
+            snake[`position${settings.player.snake + 1}`] = {
+                x: settings.player.x,
+                y: settings.player.y - 50
+            }
+            snakeDB.total = snakeDB.total + 1
+
+            settings.player.y = settings.player.y - 50
+            game("fruitDetector");
+        }
+        if (key == "d") {
+            if (settings.player.x == settings.map.width - 50) return gameValue = gameValue + 1, endGame("player_lost");
+
+            snakeSystem();
+            game("setPixel", settings.player.x + 50, settings.player.y, settings.color.player)
+
+            settings.player.snake = settings.player.snake + 1
+            snake[`position${settings.player.snake + 1}`] = {
+                x: settings.player.x + 50,
+                y: settings.player.y
+            }
+            snakeDB.total = snakeDB.total + 1
+
+            settings.player.x = settings.player.x + 50
+            game("fruitDetector");
+        }
+        if (key == "a") {
+            if (settings.player.x == 0) return gameValue = gameValue + 1, endGame("player_lost");
+
+            snakeSystem();
+            game("setPixel", settings.player.x - 50, settings.player.y, settings.color.player)
+
+            settings.player.snake = settings.player.snake + 1
+            snake[`position${settings.player.snake + 1}`] = {
+                x: settings.player.x - 50,
+                y: settings.player.y
+            }
+            snakeDB.total = snakeDB.total + 1
+
+            settings.player.x = settings.player.x - 50
+            game("fruitDetector");
+        }
+    }
+
+    //snake growing system
+    function snakeSystem() {
+        const getData = snake[`position${settings.player.snake - settings.player.points + 1}`]
+        game("clearPixel", getData.x, getData.y)
+    }
+
+    //spawn fruits
+    function fruitSystem() {
+        if (fruitsDB.ready == true) {
+            if (fruitsDB.count !== 0) {
+                const getFruitID = fruitsDB.count
+                const requestFruit = fruits[`fruit${getFruitID}`]
+
+                game("setPixel", requestFruit.x, requestFruit.y, '#ff7393')
+
+                fruitsDB.count = fruitsDB.count - 1
+                fruitSystem();
+            }
         } else {
-            fruitsDB.ready = true;
-            fruitSystem()
+            if (settings.fruits.total !== 0) {
+                settings.fruits.total = settings.fruits.total - 1
+                fruitsDB.count = fruitsDB.count + 1
+
+                fruits[`fruit${fruitsDB.count}`] = {
+                    x: Math.floor(Math.random() * settings.map.height / 50) * 50,
+                    y: Math.floor(Math.random() * settings.map.height / 50) * 50
+                }
+                fruitSystem()
+            } else {
+                fruitsDB.ready = true;
+                fruitSystem()
+            }
         }
     }
-}
-fruitSystem();
+    fruitSystem();
 
+    //auto walk when press wasd
+    function autoWalk() {
+        setTimeout(() => {
+            keyboardListener(settings.keyboard.lastKey)
+            autoWalk();
+        }, settings.player.velocity)
+    }
+    autoWalk();
+
+    //player spawn
+    canvas.fillStyle = settings.color.player;
+    canvas.fillRect(settings.player.x, settings.player.y, 50, 50);
+
+    snakeDB.total = snakeDB.total + 1
+    snake[`position${settings.player.snake + 1}`] = {
+        x: settings.player.x,
+        y: settings.player.y
+    }
+}
+play();
 
 document.addEventListener('keydown', (event) => {
-    settings.keyboard.lastKey = event.key
+    if (["w", "a", "s", "d"].includes(event.key)) settings.keyboard.lastKey = event.key
 })
-
-//player spawn
-canvas.fillStyle = settings.color.player;
-canvas.fillRect(settings.player.x, settings.player.y, 50, 50);
